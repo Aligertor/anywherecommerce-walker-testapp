d8c996dc49f3e6dfc31a821f202c4202da7566c6
Gerd Heescher <g.heescher@code-b.com>
2016-03-03 14:23:34 +0100

    - #DBF-147 init plugin

--- src/app/containers/HomeContainer.jsx
+++ src/app/containers/HomeContainer.jsx
@@ -1,12 +1,40 @@
+/* global anywherecommercecardreader */
+
 import React from 'react';
 import i18next from 'i18next';
 
 import styles from '../styles/page.scss';
 
 class HomeContainer extends React.Component {
+
+	constructor(props) {
+	    super(props);
+	    this.state = {
+	    	settingsRetrieved: false
+	    };
+	  }
+
+
+	componentDidMount() {
+
+		anywherecommercecardreader.addListener(this.onSettingsRetrieved, 'onSettingsRetrieved');
+
+		if(window.anywherecommercecardreader){
+			anywherecommercecardreader.initReader('1002', 'secretpass', true);
+		}	
+	}
+
+	onSettingsRetrieved() {
+		this.setState({
+			settingsRetrieved: true
+		});
+	}
+
 	render() {
 		return (
-			<div className="page">{i18next.t('hello_home')}</div>
+			<div className="page">
+				{this.state.settingsRetrieved && <div>Terminal connected</div>}
+			</div>
 		);
 	}
 }
